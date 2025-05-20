a8593e724fc8e3b6cb1805ec977cbcc6b7c472eb
nourmag <nourmag@yahoo.com>
2025-05-20 20:05:10 +0300

    FastAPI Adoption

--- recommender/api/main.py
+++ recommender/api/main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from recommender.ai.engine import get_recommendations_for_user
 
+#uvicorn recommender.api.main:app --reload
 app = FastAPI()
 
-class UserRequest(BaseModel):
-    user_id: int
+@app.get("/")
+def root():
+    return {"message": "Furnisique Recommendation API is running!"}
 
-@app.post("/recommendations")
-def recommend(user: UserRequest):
+@app.get("/recommend/{user_id}")
+def recommend(user_id: int):
     try:
-        recommendations = get_recommendations_for_user(user.user_id)
-        return {"user_id": user.user_id, "recommendations": recommendations}
+        recommendations = get_recommendations_for_user(user_id)
+        return {"user_id": user_id, "recommendations": recommendations}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-@app.get("/")
-def read_root():
-    return {"message": "Furnisique Recommendation API is running!"}
